131b1d4fcc2b1426b5fce6e7f57a7cce002516bd
Kevin O'Connell <kevinoconnell42@gmail.com>
2024-02-03 21:37:50 -0500

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,6 @@ import { APIDisclaimer } from "@/components/APIDisclaimer";
 import { headers } from "next/headers";
 
 export async function generateMetadata({ params }: { params: any }) {
-  const headersList = headers();
-
-  const host = headersList.get("host"); // to get domain
-
-  console.log({ host });
-
   return {
     title: "Hubscout",
     description: "Semantic search for Farcaster",
@@ -24,10 +18,10 @@ export async function generateMetadata({ params }: { params: any }) {
       "fc:frame": "vNext",
       "fc:frame:image": `/hubscout.png`,
       "fc:frame:button:1": "SEARCH",
-      "fc:frame:post_url": `${host ?? "https://www.hubscout.xyz"}/api/frame`,
+      "fc:frame:post_url": `${"https://www.hubscout.xyz"}/api/frame`,
       "fc:frame:input:text": "Search",
     },
-    metadataBase: new URL(host ?? "https://www.hubscout.xyz"),
+    metadataBase: new URL("https://www.hubscout.xyz"),
   };
 }
 
