5f4b97bdf5badfb5887100eff530fe8abb870b1c
Kevin O'Connell <kevinoconnell42@gmail.com>
2024-03-13 16:48:19 -0400

    Update route.ts

--- src/app/api/blocked/route.ts
+++ src/app/api/blocked/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(
   _: NextRequest,
   res: NextResponse,
-  { params: {} }: { params: {} },
+  {}: {},
 ) {
   return NextResponse.json("Too many requests", { status: 429 });
 }
