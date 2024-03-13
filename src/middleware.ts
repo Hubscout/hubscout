ef6ac10259b5d8e65ed8ab51b2eb4a7d3d0402f2
Kevin O'Connell <kevinoconnell42@gmail.com>
2024-03-13 16:57:02 -0400

    rate limit

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import { kv } from "@vercel/kv";
 const ratelimit = new Ratelimit({
   redis: kv,
   // 5 requests from the same IP in 10 seconds
-  limiter: Ratelimit.slidingWindow(1, "2 m"),
+  limiter: Ratelimit.slidingWindow(5, "10 s"),
 });
 
 // Define which routes you want to rate limit
@@ -16,11 +16,9 @@ export const config = {
 export default async function middleware(request: NextRequest) {
   // You could alternatively limit based on user ID or similar
   const ip = request.ip ?? "127.0.0.1";
-  console.log("ip", ip);
   const { success, pending, limit, reset, remaining } = await ratelimit.limit(
     ip,
   );
-  console.log({ success, pending, limit, reset, remaining });
   return success ? NextResponse.next() : NextResponse.redirect(
     new URL("https://www.hubscout.xyz/api/blocked"),
   );
