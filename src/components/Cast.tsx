ba7f79b6f0d9e6c0e9db760e5085128e7fecf663
Kevin O'Connell <kevinoconnell42@gmail.com>
2024-03-16 14:25:00 -0400

    revert cast changs

--- src/components/Cast.tsx
+++ src/components/Cast.tsx
@@ -14,13 +14,8 @@ export function Cast({ parent, ...cast }: CastWithPossibleParent) {
 
   // need to force the author types for now -- something's fucked with the types
   const first_author = first_cast.author as any;
-
   const second_author = (second_cast?.author as any) ?? null;
-  if (second_cast && first_cast) {
-    console.log("first", first_cast);
-    console.log("second", second_cast);
-    console.log("cast", first_cast.hash === second_cast.hash);
-  }
+
   const firstProps = {
     avatar: first_author.pfp_url as any,
     displayName: first_author.display_name,
@@ -41,15 +36,14 @@ export function Cast({ parent, ...cast }: CastWithPossibleParent) {
   };
 
   return (
-    // <Link
-    //   target="_blank"
-    //   // href={`https://warpcast.com/${first_author.username}/${first_cast.hash.slice(0, 8)}`}
-    //   className="border-2 border-slate-100 cursor-pointer hover:border-slate-200 w-full p-3 rounded-xl"
-    // >
-    <div className="border-2 border-slate-100 cursor-pointer hover:border-slate-200 w-full p-3 rounded-xl">
+    <Link
+      target="_blank"
+      href={`https://warpcast.com/${first_author.username}/${first_cast.hash.slice(0, 8)}`}
+      className="border-2 border-slate-100 cursor-pointer hover:border-slate-200 w-full p-3 rounded-xl"
+    >
       {first_cast && <CastSnippet {...firstProps} />}
       {second_cast && <CastSnippet {...secondProps} />}
-    </div>
+    </Link>
   );
 }
 
