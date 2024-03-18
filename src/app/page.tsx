f17a53b81ef3a6919b3a3e76b0f1299d9b815f6d
Kevin O'Connell <kevinoconnell42@gmail.com>
2024-03-17 20:54:21 -0400

    remove frame

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,22 +9,6 @@ import { headers } from "next/headers";
 export default async function Page() {
   return (
     <>
-      <meta property="og:image" content="https://www.hubscout.xyz/api/image" />
-      <meta property="og:title" content="Hubscout" />
-      <meta property="og:description" content="Semantic search for Farcaster" />
-      <meta property="fc:frame" content="vNext" />
-      <meta
-        property="fc:frame:image"
-        content="https://www.hubscout.xyz/api/image"
-      />
-      <meta property="fc:frame:input:text" content="Search" />
-      <meta property="fc:frame:button:1" content="SEARCH" />
-      <meta property="fc:frame:button:1:action" content="post" />
-      <meta
-        property="fc:frame:post_url"
-        content="https://www.hubscout.xyz/api/frame"
-      />
-
       <div className="w-screen h-screen p-2 col-fs-c bg-white">
         <div className="w-full col gap-2" style={{ maxWidth: 540 }}>
           <SearchBar initValue="" />
