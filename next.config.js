bcb87a635a23f1ecc657ad3f1c926da750e71d19
Kevin O'Connell <kevinoconnell42@gmail.com>
2024-04-10 20:29:59 -0700

    fix variables

--- next.config.js
+++ next.config.js
@@ -1,17 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    DATABASE_URL: process.env.DATABASE_URL,
-    RAILWAY_URL: process.env.RAILWAY_URL,
     NEYNAR_API_KEY: process.env.NEYNAR_API_KEY,
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-    POSTHOG_URL: process.env.POSTHOG_URL,
-    PROFILES_NAME: process.env.PROFILES_NAME,
-    COLLECTION_NAME: process.env.COLLECTION_NAME,
     URL: process.env.URL,
     SUPABASE_URL: process.env.SUPABASE_URL,
     SUPABASE_KEY: process.env.SUPABASE_KEY,
-    NEXT_PUBLIC_AMPLITUDE_API: process.env.NEXT_PUBLIC_AMPLITUDE_API,
+    AMPLITUDE_API: process.env.AMPLITUDE_API,
   },
 };
 
--- src/lib/chroma.ts
+++
@@ -1,8 +0,0 @@
-import { OpenAIEmbeddingFunction } from "chromadb";
-import { ChromaClient } from "chromadb";
-
-const openai_api_key = process.env.OPENAI_API_KEY as string;
-const path = process.env.RAILWAY_URL as string;
-
-export const embedder = new OpenAIEmbeddingFunction({ openai_api_key });
-export const client = new ChromaClient({ path });
